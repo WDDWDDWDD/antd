c5fb01fd6deae86ef3073b7eea4db1f6db57722e
魏冬冬 <1406282241@qq.com>
2019-03-18 09:47:12 +0800

    first commit

---
+++ src/redux/reducers/todos.js
@@ -0,0 +1,30 @@
+let state = [
+  {
+    id:0,
+    text:'222',
+    completed: false 
+  }
+]
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map(todo =>
+        (todo.id === action.id) 
+          ? {...todo, completed: !todo.completed}
+          : todo
+      )
+    default:
+      return state
+  }
+}
+
+export default todos
